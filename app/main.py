fefccaf88b8b78db4edc1caec0082ffa3aa920d8
gitelvispro1993 <jagenelvis@gmail.com>
2026-01-18 02:22:59 +0200

    fix nginix handling two domain name

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 from .routers import public, admin, leads # Refactored imports
 
+from fastapi.responses import FileResponse
+import os
 
 
 # This command uses Base to create the tables in Postgres
@@ -28,12 +30,28 @@ app.include_router(leads.router)
 # Serve the high-end Frontend
 from fastapi.responses import FileResponse
 
+
+# Get the directory where main.py is located
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 # 1. Access for Main WebApp
 @app.get("/")
 async def read_index():
     return FileResponse('index.html')
 
+
 # 2. Access for Admin Dashboard
 @app.get("/portal-access-admin")
 async def read_admin():
-    return FileResponse('admin.html')
+    # Use join to be safe about paths
+    admin_path = os.path.join(BASE_DIR, "admin.html")
+    if not os.path.exists(admin_path):
+        return {"error": "Admin file not found on server"}
+    return FileResponse(admin_path)
+
+
+
+
+
+
